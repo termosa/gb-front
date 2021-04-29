e7ce09dd988cff5dc18bbc5c2c32e55d08583d02
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-04-29 13:54:19 +0300

    Create generator

---
+++ gravity-brands/generators/component/component.aspect.ts
@@ -0,0 +1,5 @@
+import { Aspect } from '@teambit/harmony'
+
+export const ComponentAspect = Aspect.create({
+  id: 'gravity-brands/generators/component',
+})
---
+++ gravity-brands/generators/component/index.ts
@@ -0,0 +1,5 @@
+import { ComponentAspect } from './component.aspect'
+
+export type { ComponentMain } from './component.main.runtime'
+export default ComponentAspect
+export { ComponentAspect }
