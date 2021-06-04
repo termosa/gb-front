6996a7e1f90ee6e8c62bbe06a6d96eb1d26a8e03
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-06-04 10:28:26 +0300

    Add trending-section

---
+++ gravity-brands/components/trending-section/index.ts
@@ -0,0 +1,2 @@
+export * from './trending-section'
+export { TrendingSection as default } from './trending-section'
--- gravity-brands/helpers/theme-provider/theme.ts
+++ gravity-brands/helpers/theme-provider/theme.ts
@@ -1,4 +1,7 @@
 const theme = {
+  fonts: {
+    familyMain: 'Montserrat, sans-serif',
+  },
   colors: {
     black: '#000000',
     white: '#ffffff',
