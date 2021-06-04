e06097ed620bd8114ead1b3fe3efc1388e938583
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-06-04 14:20:47 +0300

    Properly install dependencies on remote branch

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+# Add token so Yarn can install deps from Bit
+echo '//node.bit.dev/:_authToken=${BIT_TOKEN}' >> .npmrc
+
+# Install deps using Yarn
+yarn
+
+# Remove the line that was added so Git doesn't track that change
+sed -i '' '/BIT_TOKEN/d' .npmrc
