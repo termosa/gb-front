773c855b7c53cc6019fb17a6cdccf0cfc4e7ef41
yostarlord <82102319+yostarlord@users.noreply.github.com>
2021-07-09 13:59:05 +0300

    information overview component added

---
+++ gravity-brands/components/information-overview/information-overview.composition.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PresentationCanvas from '@fragrantjewels/gravity-brands.helpers.presentation-canvas'
+import { InformationOverview } from './information-overview'
+
+export const BasicInformationCard = (): React.ReactElement => (
+  <PresentationCanvas>
+    <InformationOverview
+      titleFirst={'The most fun you’ve'}
+      titleSecond={'ever had relaxing'}
+      cards={[
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+      ]}
+    />
+  </PresentationCanvas>
+)
