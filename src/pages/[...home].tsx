846784bff31178597a154ab3ad0c1c2ebd298fdf
yostarlord <82102319+yostarlord@users.noreply.github.com>
2021-07-12 16:48:32 +0300

    builder integration

--- src/pages/[...home].tsx
+++ src/pages/[...home].tsx
@@ -1,20 +1,21 @@
 import { builder, Builder } from '@builder.io/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
-import { CategoryShopCard } from '@fragrantjewels/gravity-brands.components.category-shop-card/category-shop-card'
+import { Header } from '@fragrantjewels/gravity-brands.components.header'
 import builderIoProps from './resolvers/builderIoProps'
 import builderIoPaths from './resolvers/builderIoPaths'
 import BuilderIoPage from './BuilderIoPage'
-import { PromiseBar } from '@fragrantjewels/gravity-brands.components.promise-bar/promise-bar'
-import { HeroGallery } from '@fragrantjewels/gravity-brands.components.hero-gallery/hero-gallery'
-import { TrendingSection } from '@fragrantjewels/gravity-brands.components.trending-section/trending-section'
-import { InnerCircleExclusive } from '../../gravity-brands/components/inner-circle-exclusive'
-import { ReviewsSection } from '@fragrantjewels/gravity-brands.components.reviews-section/reviews-section'
-import { ShopByProductsOverview } from '@fragrantjewels/gravity-brands.components.shop-by-products-overview/shop-by-products-overview'
-import { InlineSignupForm } from '@fragrantjewels/gravity-brands.components.inline-signup-form/inline-signup-form'
-import { Footer } from '@fragrantjewels/gravity-brands.components.footer/footer'
-import { Header } from '../../gravity-brands/components/header'
+import config from 'config/builder'
+import { HeroGallery } from '@fragrantjewels/gravity-brands.components.hero-gallery'
+import { InformationOverview } from '@fragrantjewels/gravity-brands.components.information-overview'
+import { CategoryShopCard } from '@fragrantjewels/gravity-brands.components.category-shop-card'
+import { ReviewsSection } from '@fragrantjewels/gravity-brands.components.reviews-section'
+import { ShopByProductsOverview } from '@fragrantjewels/gravity-brands.components.shop-by-products-overview'
+import { PromiseBar } from '@fragrantjewels/gravity-brands.components.promise-bar'
+import { InlineSignupForm } from '@fragrantjewels/gravity-brands.components.inline-signup-form'
+import { FollowUs } from '../../gravity-brands/components/follow-us'
+import { Footer } from '@fragrantjewels/gravity-brands.components.footer'
 
-builder.init('11f0313bca7a437dbb8b37ee02351718')
+builder.init(config.apiKey)
 
 Builder.registerComponent(Header, {
   name: 'Header',
@@ -29,36 +30,131 @@ Builder.registerComponent(Header, {
 
 Builder.registerComponent(HeroGallery, {
   name: 'Hero Gallery',
-})
-
-Builder.registerComponent(TrendingSection, {
-  name: 'Trending Section',
   inputs: [
     {
-      name: 'products',
-      type: 'object',
-      defaultValue: '',
+      name: 'slides',
+      type: 'list',
+      subFields: [
+        {
+          name: 'topText',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'centerFirstText',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'centerSecondText',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'bottomText',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'buttonText',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'buttonLink',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'backgroundImg',
+          type: 'string',
+          defaultValue: '',
+        },
+      ],
+      defaultValue: [
+        {
+          topText: 'MINISUBLINER',
+          centerFirstText: 'SAVOR the',
+          centerSecondText: 'quiet MOMENTS',
+          bottomText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque.',
+          buttonText: 'SHOP NOW',
+          buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+          backgroundImg: '',
+        },
+      ],
     },
   ],
 })
 
-Builder.registerComponent(InnerCircleExclusive, {
-  name: 'Inner Circle Exclusive',
+Builder.registerComponent(CategoryShopCard, {
+  name: 'Category Shop Card',
   inputs: [
     {
-      name: 'productTitle',
+      name: 'image',
       type: 'string',
-      defaultValue: 'Moon Magic',
+      defaultValue: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
     },
     {
-      name: 'price',
+      name: 'title',
       type: 'string',
-      defaultValue: '$35.49',
+      defaultValue: 'Title',
     },
     {
-      name: 'discountPrice',
+      name: 'subtitle',
       type: 'string',
-      defaultValue: '$30.99',
+      defaultValue: 'Subtitle',
+    },
+    {
+      name: 'link',
+      type: 'string',
+      defaultValue: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+    },
+  ],
+})
+
+Builder.registerComponent(InformationOverview, {
+  name: 'Information Overview',
+  inputs: [
+    {
+      name: 'cards',
+      type: 'list',
+      subFields: [
+        {
+          name: 'image',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'title',
+          type: 'string',
+          defaultValue: '',
+        },
+        {
+          name: 'description',
+          type: 'string',
+          defaultValue: '',
+        },
+      ],
+      defaultValue: [
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+        {
+          image: 'https://i.ibb.co/QFSL0Xd/Background.jpg',
+          title: 'Info Card Title',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent convallis.',
+        },
+      ],
     },
   ],
 })
@@ -66,11 +162,6 @@ Builder.registerComponent(InnerCircleExclusive, {
 Builder.registerComponent(ReviewsSection, {
   name: 'Review Section',
   inputs: [
-    {
-      name: 'images',
-      type: 'string',
-      defaultValue: '',
-    },
     {
       name: 'quote',
       type: 'string',
@@ -82,26 +173,38 @@ Builder.registerComponent(ReviewsSection, {
       type: 'string',
       defaultValue: 'Samanta H.',
     },
+    {
+      name: 'images',
+      type: 'list',
+      defaultValue: [
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/buzzfeed.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/cosmopolitan.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/refinery29.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/allure.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/beauty-insider.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/popsugar.png',
+      ],
+    },
   ],
 })
 
-Builder.registerComponent(CategoryShopCard, {
-  name: 'Category Shop Card',
+Builder.registerComponent(ShopByProductsOverview, {
+  name: 'Shop By Product Overview',
   inputs: [
     {
       name: 'image',
-      type: 'file',
-      defaultValue: '',
+      type: 'string',
+      defaultValue: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-1.jpg',
+    },
+    {
+      name: 'buttonLink',
+      type: 'string',
+      defaultValue: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
     },
     {
       name: 'title',
       type: 'string',
-      defaultValue: 'Sample Title',
-    },
-    {
-      name: 'subTitle',
-      type: 'string',
-      defaultValue: 'Sample Subtitle',
+      defaultValue: 'Title',
     },
   ],
 })
@@ -110,35 +213,19 @@ Builder.registerComponent(PromiseBar, {
   name: 'Promise Bar',
 })
 
-Builder.registerComponent(ShopByProductsOverview, {
-  name: 'Shop By Product Section',
+Builder.registerComponent(InlineSignupForm, {
+  name: 'Inline Signup Form',
   inputs: [
     {
-      name: 'products',
-      type: 'list',
-      defaultValue: [
-        { image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-1.jpg', product_id: 1 },
-        { image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-2.jpg', product_id: 2 },
-        { image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-3.jpg', product_id: 3 },
-      ],
-      subFields: [
-        {
-          name: 'image',
-          type: 'file',
-          defaultValue: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-1.jpg',
-        },
-        {
-          name: 'product_id',
-          type: 'string',
-          defaultValue: 1,
-        },
-      ],
+      name: 'onSignup',
+      type: 'func',
+      defaultValue: '(email) => console.log(email)',
     },
   ],
 })
 
-Builder.registerComponent(InlineSignupForm, {
-  name: 'Inline Signup Form',
+Builder.registerComponent(FollowUs, {
+  name: 'Follow Us',
 })
 
 Builder.registerComponent(Footer, {
