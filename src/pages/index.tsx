4f338d6a1a7b564b0b1cbe95ab938230ec36eb6d
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-07-22 19:26:05 +0300

    Update MainPageLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,116 +23,108 @@ type HomePageProps = {
 
 const HomePage = ({ trendingProducts, innerCircleProduct }: HomePageProps): React.ReactElement => (
   <MainPageLayout>
-    <div className="app-re-wrapper" id="app-wrapper">
-      <div className="app-re-content" id="app-content">
-        <main className="app-h-main">
-          <HeroGallery
-            slides={[
-              {
-                topText: 'MINISUBLINER',
-                centerFirstText: 'SAVOR the',
-                centerSecondText: 'MOMENTS',
-                bottomText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque.',
-                buttonText: 'SHOP NOW',
-                buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
-                backgroundImg: '',
-              },
-            ]}
-          />
-          {trendingProducts?.length && (
-            <ProductsCarousel
-              title="What’s trending"
-              subTitle="Shop our newest and best selling collections."
-              products={trendingProducts}
-              onSelectProduct={(productId) =>
-                (location.href = `/product?id=${productId}&${
-                  location.search.match(/(?:\?|&)(base_url=[^&]+)(?:&|$)/)?.[1] || ''
-                }`)
-              }
-            />
-          )}
-          {innerCircleProduct && (
-            <InnerCircleExclusive
-              product={innerCircleProduct}
-              slideImages={[
-                'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/ic-img-jewelry.jpg',
-                'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/ic-img.jpg',
-              ]}
-              buttonLink={''}
-              title={'Get addicted to me-time'}
-              subTitle={
-                'Join the Inner Circle for exciting new collections every month, available exclusively to members'
-              }
-              topButtonText={'GET STARTED'}
-              onReserve={(variant) => alert(`We will ship you the product with size ${variant.title}`)}
-            />
-          )}
-          <MostFunOverview />
+    <HeroGallery
+      slides={[
+        {
+          topText: 'MINISUBLINER',
+          centerFirstText: 'SAVOR the',
+          centerSecondText: 'MOMENTS',
+          bottomText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque.',
+          buttonText: 'SHOP NOW',
+          buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+          backgroundImg: '',
+        },
+      ]}
+    />
+    {trendingProducts?.length && (
+      <ProductsCarousel
+        title="What’s trending"
+        subTitle="Shop our newest and best selling collections."
+        products={trendingProducts}
+        onSelectProduct={(productId) =>
+          (location.href = `/product?id=${productId}&${
+            location.search.match(/(?:\?|&)(base_url=[^&]+)(?:&|$)/)?.[1] || ''
+          }`)
+        }
+      />
+    )}
+    {innerCircleProduct && (
+      <InnerCircleExclusive
+        product={innerCircleProduct}
+        slideImages={[
+          'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/ic-img-jewelry.jpg',
+          'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/ic-img.jpg',
+        ]}
+        buttonLink={''}
+        title={'Get addicted to me-time'}
+        subTitle={'Join the Inner Circle for exciting new collections every month, available exclusively to members'}
+        topButtonText={'GET STARTED'}
+        onReserve={(variant) => alert(`We will ship you the product with size ${variant.title}`)}
+      />
+    )}
+    <MostFunOverview />
 
-          <div className="app-h-section app-h-collections-section">
-            <div className="app-h-container">
-              <div className="app-h-container__content">
-                <CategoryShopCard
-                  image="https://fragrantjewels.s3.amazonaws.com/app/app-home/img/collection-img-1.jpg"
-                  title="Valentine’s Shop"
-                  subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                  link="'https://www.fragrantjewels.com/pages/inner-circle#how-it-works'"
-                />
-                <CategoryShopCard
-                  image="https://fragrantjewels.s3.amazonaws.com/app/app-home/img/collection-img-2.jpg"
-                  title="Mythical Shop"
-                  subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                  link="'https://www.fragrantjewels.com/pages/inner-circle#how-it-works'"
-                />
-              </div>
-            </div>
-          </div>
-
-          <ReviewsSection
-            images={[
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/buzzfeed.png',
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/cosmopolitan.png',
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/refinery29.png',
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/allure.png',
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/beauty-insider.png',
-              'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/popsugar.png',
-            ]}
-            quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            author="Samanta H."
+    <div className="app-h-section app-h-collections-section">
+      <div className="app-h-container">
+        <div className="app-h-container__content">
+          <CategoryShopCard
+            image="https://fragrantjewels.s3.amazonaws.com/app/app-home/img/collection-img-1.jpg"
+            title="Valentine’s Shop"
+            subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            link="'https://www.fragrantjewels.com/pages/inner-circle#how-it-works'"
           />
-          <ShopByProductsOverview
-            products={[
-              {
-                image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-1.jpg',
-                buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
-                title: 'CANDLES',
-              },
-              {
-                image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-2.jpg',
-                buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
-                title: 'BODY SCRUBS',
-              },
-              {
-                image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-3.jpg',
-                buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
-                title: 'BUNDLES',
-              },
-            ]}
+          <CategoryShopCard
+            image="https://fragrantjewels.s3.amazonaws.com/app/app-home/img/collection-img-2.jpg"
+            title="Mythical Shop"
+            subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            link="'https://www.fragrantjewels.com/pages/inner-circle#how-it-works'"
           />
-          <PromiseBar />
-          <section className="app-h-section app-h-form-section">
-            <div className="app-h-container">
-              <InlineSignupForm onSignup={signup} />
-            </div>
-          </section>
-          <section className="app-h-section app-h-follow-section">
-            <div className="app-h-container">
-              <FollowUs />
-            </div>
-          </section>
-        </main>
+        </div>
       </div>
     </div>
+
+    <ReviewsSection
+      images={[
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/buzzfeed.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/cosmopolitan.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/refinery29.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/allure.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/beauty-insider.png',
+        'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/popsugar.png',
+      ]}
+      quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. At eget iaculis eget eget neque, posuere quis placerat arcu. Ipsum est felis varius faucibus praesent.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      author="Samanta H."
+    />
+    <ShopByProductsOverview
+      products={[
+        {
+          image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-1.jpg',
+          buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+          title: 'CANDLES',
+        },
+        {
+          image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-2.jpg',
+          buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+          title: 'BODY SCRUBS',
+        },
+        {
+          image: 'https://fragrantjewels.s3.amazonaws.com/app/app-home/img/shop-by-product-img-3.jpg',
+          buttonLink: 'https://www.fragrantjewels.com/pages/inner-circle#how-it-works',
+          title: 'BUNDLES',
+        },
+      ]}
+    />
+    <PromiseBar />
+    <section className="app-h-section app-h-form-section">
+      <div className="app-h-container">
+        <InlineSignupForm onSignup={signup} />
+      </div>
+    </section>
+    <section className="app-h-section app-h-follow-section">
+      <div className="app-h-container">
+        <FollowUs />
+      </div>
+    </section>
   </MainPageLayout>
 )
 
