20547ff64a0845ae5847c3308aec064eb8fd11c2
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-07-22 19:20:11 +0300

    Fix env parsing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,10 @@ import { ThemeProvider } from '@fragrantjewels/gravity-brands.helpers.theme-prov
 import './styles/product.scss'
 import './styles/styles.scss'
 
-const API_URL_PROPERTY_NAME = typeof window === 'undefined' ? 'BASE_API_URL' : 'CLIENT_API_URL'
+const apiUrl = typeof window === 'undefined' ? process.env.BASE_API_URL : process.env.CLIENT_API_URL
 
-if (!baseApiUrl(process.env[API_URL_PROPERTY_NAME])) {
-  throw new Error(`${API_URL_PROPERTY_NAME} env variable is not set`)
+if (!baseApiUrl(apiUrl)) {
+  throw new Error(`${typeof window === 'undefined' ? 'BASE' : 'CLIENT'}_API_URL env variable is not set`)
 }
 
 const CustomApp: FC<AppProps> = ({ Component, pageProps }) => (
