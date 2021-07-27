95eec3efa348ce11a4d9ed74f1a4549799e0c6eb
alexandrkovalhygge <86121465+alexandrkovalhygge@users.noreply.github.com>
2021-07-27 14:50:11 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,13 +2,40 @@ tee patch.yaml 2>&1 >> /dev/null <<- EOF
 apiVersion: apps/v1
 kind: Deployment
 metadata:
+  labels:
+    app: front
   name: front
   namespace: default
 spec:
+  progressDeadlineSeconds: 600
+  replicas: 1
+  revisionHistoryLimit: 3
+  selector:
+    matchLabels:
+      app: front
+  strategy:
+    rollingUpdate:
+      maxSurge: 25%
+      maxUnavailable: 25%
+    type: RollingUpdate
   template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        app: front
     spec:
       containers:
-      - image: $1
+        image: ${1}
         imagePullPolicy: Always
         name: front
+        ports:
+        - containerPort: 3000
+          protocol: TCP
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+      dnsPolicy: ClusterFirst
+      restartPolicy: Always
+      schedulerName: default-scheduler
+      securityContext: {}
+      terminationGracePeriodSeconds: 30
 EOF
