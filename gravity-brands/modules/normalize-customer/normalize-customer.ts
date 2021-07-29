ea21f331facf769c8a02e1a90d1ba63f20eb8a46
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-07-29 11:54:07 +0300

    Update versions

--- gravity-brands/modules/normalize-customer/normalize-customer.ts
+++ gravity-brands/modules/normalize-customer/normalize-customer.ts
@@ -1,27 +1,27 @@
 import { ServerCustomerAddress } from '@fragrantjewels/gravity-brands.modules.normalize-customer-address'
 
 export enum TaxExemption {
-  'EXEMPT_ALL' = 'EXEMPT_ALL', // This customer is exempt from all Canadian taxes.
-  'CA_STATUS_CARD_EXEMPTION' = 'CA_STATUS_CARD_EXEMPTION', // This customer is exempt from specific taxes for holding a valid STATUS_CARD_EXEMPTION in Canada.
-  'CA_DIPLOMAT_EXEMPTION' = 'CA_DIPLOMAT_EXEMPTION', // This customer is exempt from specific taxes for holding a valid DIPLOMAT_EXEMPTION in Canada.
-  'CA_BC_RESELLER_EXEMPTION' = 'CA_BC_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in British Columbia.
-  'CA_MB_RESELLER_EXEMPTION' = 'CA_MB_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in Manitoba.
-  'CA_SK_RESELLER_EXEMPTION' = 'CA_SK_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in Saskatchewan.
-  'CA_BC_COMMERCIAL_FISHERY_EXEMPTION' = 'CA_BC_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in British Columbia.
-  'CA_MB_COMMERCIAL_FISHERY_EXEMPTION' = 'CA_MB_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Manitoba.
-  'CA_NS_COMMERCIAL_FISHERY_EXEMPTION' = 'CA_NS_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Nova Scotia.
-  'CA_PE_COMMERCIAL_FISHERY_EXEMPTION' = 'CA_PE_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Prince Edward Island.
-  'CA_SK_COMMERCIAL_FISHERY_EXEMPTION' = 'CA_SK_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Saskatchewan.
-  'CA_BC_PRODUCTION_AND_MACHINERY_EXEMPTION' = 'CA_BC_PRODUCTION_AND_MACHINERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PRODUCTION_AND_MACHINERY_EXEMPTION in British Columbia.
-  'CA_SK_PRODUCTION_AND_MACHINERY_EXEMPTION' = 'CA_SK_PRODUCTION_AND_MACHINERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PRODUCTION_AND_MACHINERY_EXEMPTION in Saskatchewan.
-  'CA_BC_SUB_CONTRACTOR_EXEMPTION' = 'CA_BC_SUB_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid SUB_CONTRACTOR_EXEMPTION in British Columbia.
-  'CA_SK_SUB_CONTRACTOR_EXEMPTION' = 'CA_SK_SUB_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid SUB_CONTRACTOR_EXEMPTION in Saskatchewan.
-  'CA_BC_CONTRACTOR_EXEMPTION' = 'CA_BC_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid CONTRACTOR_EXEMPTION in British Columbia.
-  'CA_SK_CONTRACTOR_EXEMPTION' = 'CA_SK_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid CONTRACTOR_EXEMPTION in Saskatchewan.
-  'CA_ON_PURCHASE_EXEMPTION' = 'CA_ON_PURCHASE_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PURCHASE_EXEMPTION in Ontario.
-  'CA_MB_FARMER_EXEMPTION' = 'CA_MB_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Manitoba.
-  'CA_NS_FARMER_EXEMPTION' = 'CA_NS_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Nova Scotia.
-  'CA_SK_FARMER_EXEMPTION' = 'CA_SK_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Saskatchewan.
+  EXEMPT_ALL = 'EXEMPT_ALL', // This customer is exempt from all Canadian taxes.
+  CA_STATUS_CARD_EXEMPTION = 'CA_STATUS_CARD_EXEMPTION', // This customer is exempt from specific taxes for holding a valid STATUS_CARD_EXEMPTION in Canada.
+  CA_DIPLOMAT_EXEMPTION = 'CA_DIPLOMAT_EXEMPTION', // This customer is exempt from specific taxes for holding a valid DIPLOMAT_EXEMPTION in Canada.
+  CA_BC_RESELLER_EXEMPTION = 'CA_BC_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in British Columbia.
+  CA_MB_RESELLER_EXEMPTION = 'CA_MB_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in Manitoba.
+  CA_SK_RESELLER_EXEMPTION = 'CA_SK_RESELLER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid RESELLER_EXEMPTION in Saskatchewan.
+  CA_BC_COMMERCIAL_FISHERY_EXEMPTION = 'CA_BC_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in British Columbia.
+  CA_MB_COMMERCIAL_FISHERY_EXEMPTION = 'CA_MB_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Manitoba.
+  CA_NS_COMMERCIAL_FISHERY_EXEMPTION = 'CA_NS_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Nova Scotia.
+  CA_PE_COMMERCIAL_FISHERY_EXEMPTION = 'CA_PE_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Prince Edward Island.
+  CA_SK_COMMERCIAL_FISHERY_EXEMPTION = 'CA_SK_COMMERCIAL_FISHERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid COMMERCIAL_FISHERY_EXEMPTION in Saskatchewan.
+  CA_BC_PRODUCTION_AND_MACHINERY_EXEMPTION = 'CA_BC_PRODUCTION_AND_MACHINERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PRODUCTION_AND_MACHINERY_EXEMPTION in British Columbia.
+  CA_SK_PRODUCTION_AND_MACHINERY_EXEMPTION = 'CA_SK_PRODUCTION_AND_MACHINERY_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PRODUCTION_AND_MACHINERY_EXEMPTION in Saskatchewan.
+  CA_BC_SUB_CONTRACTOR_EXEMPTION = 'CA_BC_SUB_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid SUB_CONTRACTOR_EXEMPTION in British Columbia.
+  CA_SK_SUB_CONTRACTOR_EXEMPTION = 'CA_SK_SUB_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid SUB_CONTRACTOR_EXEMPTION in Saskatchewan.
+  CA_BC_CONTRACTOR_EXEMPTION = 'CA_BC_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid CONTRACTOR_EXEMPTION in British Columbia.
+  CA_SK_CONTRACTOR_EXEMPTION = 'CA_SK_CONTRACTOR_EXEMPTION', // This customer is exempt from specific taxes for holding a valid CONTRACTOR_EXEMPTION in Saskatchewan.
+  CA_ON_PURCHASE_EXEMPTION = 'CA_ON_PURCHASE_EXEMPTION', // This customer is exempt from specific taxes for holding a valid PURCHASE_EXEMPTION in Ontario.
+  CA_MB_FARMER_EXEMPTION = 'CA_MB_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Manitoba.
+  CA_NS_FARMER_EXEMPTION = 'CA_NS_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Nova Scotia.
+  CA_SK_FARMER_EXEMPTION = 'CA_SK_FARMER_EXEMPTION', // This customer is exempt from specific taxes for holding a valid FARMER_EXEMPTION in Saskatchewan.
 }
 
 // https://shopify.dev/api/admin/rest/reference/customers/customer?api%5Bversion%5D=2021-01#index-2021-01
