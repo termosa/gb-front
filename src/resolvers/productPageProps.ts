974515e3d7ad07490098c41a7af643b78a95db89
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-08-02 09:27:28 +0300

    Restore products loading

--- src/resolvers/productPageProps.ts
+++ src/resolvers/productPageProps.ts
@@ -1,14 +1,23 @@
 import { GetServerSidePropsContext } from 'next'
 import resolvePageProps from '@fragrantjewels/gravity-brands.modules.resolve-page-props'
-import loadProduct from '@fragrantjewels/gravity-brands.modules.load-product'
-import loadCollection, { Product } from '@fragrantjewels/gravity-brands.modules.load-collection'
+import loadProduct, { Product } from '@fragrantjewels/gravity-brands.modules.load-product'
+import loadCollection from '@fragrantjewels/gravity-brands.modules.load-collection'
 import removeNewLineCharacters from '@fragrantjewels/gravity-brands.modules.remove-new-line-characters'
 import { POTENTIAL_PRODUCTS_COLLECTION_ID, RECOMMENDED_PRODUCTS_COLLECTION_ID } from 'src/settings/ids'
+import loadModelTemplate, { ModelTemplate } from 'src/builder/load-model-template'
 import { parse } from 'node-html-parser'
 
 export type ProductDescription = {
   title: string
   content: string
+}
+
+export type ProductPageProps = {
+  product: Product
+  recommendedProducts: Array<Product>
+  potentialProducts: Array<Product>
+  productDescription: ProductDescription
+  builderContent: ModelTemplate
 }
 
 const loadCollectionProducts = (collectionId: number): Promise<Product[] | null> =>
@@ -34,12 +43,18 @@ const getProductDescription = async (context: GetServerSidePropsContext): Promis
 }
 
 function productPageProps<PropsType>(): (context: GetServerSidePropsContext) => Promise<{ props: PropsType }> {
-  return resolvePageProps((context) => ({
-    product: loadProduct(Number(context.query.id)).catch(() => null),
-    recommendedProducts: loadCollectionProducts(RECOMMENDED_PRODUCTS_COLLECTION_ID),
-    potentialProducts: loadCollectionProducts(POTENTIAL_PRODUCTS_COLLECTION_ID),
-    productDescription: getProductDescription(context),
-  }))
+  return resolvePageProps((context) => {
+    const productPromise = loadProduct(+context.query.productId)
+    return {
+      product: productPromise.catch(() => null),
+      recommendedProducts: loadCollectionProducts(RECOMMENDED_PRODUCTS_COLLECTION_ID),
+      potentialProducts: loadCollectionProducts(POTENTIAL_PRODUCTS_COLLECTION_ID),
+      productDescription: getProductDescription(context),
+      builderContent: productPromise
+        .then((product) => loadModelTemplate('product', product.template))
+        .catch((error) => (console.error(error), null)),
+    }
+  })
 }
 
 export default productPageProps
