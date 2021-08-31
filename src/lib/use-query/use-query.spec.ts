ee52b403fbbf76ee5b24361b2a8f73fb8f1a14d7
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-08-31 11:42:53 +0300

    fix comment, change describe name button, remove composition, chanhe name name useQuery, replace #, add test for multyply query,

--- src/lib/use-query/use-query.spec.ts
+++ src/lib/use-query/use-query.spec.ts
@@ -3,14 +3,27 @@ import useQuery from '.'
 import window from '../window'
 jest.mock('../window', () => ({}))
 
-describe('navigate()', () => {
+describe('useQuery()', () => {
   beforeEach(() => {
     Object.keys(window).forEach((key) => delete window[key])
-    Object.assign(window, { location: { href: '/initial-page' } })
   })
 
-  it('should add query "collections/jewel-candles#size=6&sortF=price-low-to-high"', () => {
-    window.location.search = '/size=6&sortF=price-low-to-high'
+  it('should parse multiply parameter empty', () => {
+    Object.assign(window, { location: { search: '' } })
+    const { result } = renderHook(() => useQuery())
+    const res = {}
+    expect(result.current).toEqual(res)
+  })
+
+  it('should parse single parameter "size=6"', () => {
+    Object.assign(window, { location: { search: '?size=6' } })
+    const { result } = renderHook(() => useQuery())
+    const res = { size: '6' }
+    expect(result.current).toEqual(res)
+  })
+
+  it('should parse multiply parameters "size=6&sortF=price-low-to-high"', () => {
+    Object.assign(window, { location: { search: '?size=6&sortF=price-low-to-high' } })
     const { result } = renderHook(() => useQuery())
     const res = { size: '6', sortF: 'price-low-to-high' }
     expect(result.current).toEqual(res)
