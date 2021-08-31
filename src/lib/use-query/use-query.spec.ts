db2c9a9e1ce0470f5a734e4c4f3f54fa42896ae9
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-08-31 12:57:12 +0300

    Move use-query and button to lib

--- src/lib/use-query/use-query.spec.ts
+++ src/lib/use-query/use-query.spec.ts
@@ -11,21 +11,18 @@ describe('useQuery()', () => {
   it('should parse empty parameter', () => {
     Object.assign(window, { location: { search: '' } })
     const { result } = renderHook(() => useQuery())
-    const res = {}
-    expect(result.current).toEqual(res)
+    expect(result.current).toEqual({})
   })
 
   it('should parse single parameter', () => {
     Object.assign(window, { location: { search: '?size=6' } })
     const { result } = renderHook(() => useQuery())
-    const res = { size: '6' }
-    expect(result.current).toEqual(res)
+    expect(result.current).toEqual({ size: '6' })
   })
 
   it('should parse multiply parameters', () => {
     Object.assign(window, { location: { search: '?size=6&sortF=price-low-to-high' } })
     const { result } = renderHook(() => useQuery())
-    const res = { size: '6', sortF: 'price-low-to-high' }
-    expect(result.current).toEqual(res)
+    expect(result.current).toEqual({ size: '6', sortF: 'price-low-to-high' })
   })
 })
--- src/lib/use-query/use-query.ts
+++ src/lib/use-query/use-query.ts
@@ -1,17 +1,16 @@
 import { useMemo } from 'react'
 import window from '../window'
 
+const parseQueryString = () => {
+  if (!window) return {}
+  return window.location.search
+    .slice(1)
+    .split('&')
+    .filter(Boolean)
+    .map((p) => p.split('='))
+    .reduce((query, [name, value]) => Object.assign(query, { [name]: value }), {} as Record<string, undefined | string>)
+}
+
 export function useQuery(): Record<string, undefined | string> {
-  return useMemo(() => {
-    if (!window) return {}
-    return window.location.search
-      .slice(1)
-      .split('&')
-      .filter(Boolean)
-      .map((p) => p.split('='))
-      .reduce(
-        (query, [name, value]) => Object.assign(query, { [name]: value }),
-        {} as Record<string, undefined | string>
-      )
-  }, [typeof window])
+  return useMemo(parseQueryString, [window?.location.search])
 }
