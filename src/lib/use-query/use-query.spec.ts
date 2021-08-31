ce71b7dc15a3cfb9a6980518b1b7442028804bd7
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-08-31 11:53:19 +0300

    rename test name

--- src/lib/use-query/use-query.spec.ts
+++ src/lib/use-query/use-query.spec.ts
@@ -8,21 +8,21 @@ describe('useQuery()', () => {
     Object.keys(window).forEach((key) => delete window[key])
   })
 
-  it('should parse multiply parameter empty', () => {
+  it('should parse empty parameter', () => {
     Object.assign(window, { location: { search: '' } })
     const { result } = renderHook(() => useQuery())
     const res = {}
     expect(result.current).toEqual(res)
   })
 
-  it('should parse single parameter "size=6"', () => {
+  it('should parse single parameter', () => {
     Object.assign(window, { location: { search: '?size=6' } })
     const { result } = renderHook(() => useQuery())
     const res = { size: '6' }
     expect(result.current).toEqual(res)
   })
 
-  it('should parse multiply parameters "size=6&sortF=price-low-to-high"', () => {
+  it('should parse multiply parameters', () => {
     Object.assign(window, { location: { search: '?size=6&sortF=price-low-to-high' } })
     const { result } = renderHook(() => useQuery())
     const res = { size: '6', sortF: 'price-low-to-high' }
