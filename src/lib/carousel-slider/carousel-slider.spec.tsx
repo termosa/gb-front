2ea820a5b3d6d74065729a769762cd42e79fd402
Dmitriy Daniuk <dmitriy.daniuk@hygge.software>
2021-09-03 12:43:06 +0300

    Docs and tests carosel-slider

--- src/lib/carousel-slider/carousel-slider.spec.tsx
+++ src/lib/carousel-slider/carousel-slider.spec.tsx
@@ -3,9 +3,16 @@ import { render } from '@testing-library/react'
 import CarouselSlider from '.'
 
 describe('<CarouselSlider>', () => {
-  it('should render with the correct text', () => {
-    const { getByText } = render(<CarouselSlider>Component content</CarouselSlider>)
-    const rendered = getByText('Component content')
-    expect(rendered).toBeTruthy()
+  it('should render with the correct image', async () => {
+    const imgSrc = 'https://source.unsplash.com/random/800x800/?img='
+    const { container } = render(
+      <CarouselSlider scrollbarPresent dotsPresent>
+        {[...new Array(5).fill(1)].map((_, i) => {
+          return <img src={imgSrc + i} alt="" style={{ width: '300px' }} key={imgSrc + i} />
+        })}
+      </CarouselSlider>
+    )
+    const displayedImage = container.querySelector('.react-multi-carousel-track ')
+    expect(displayedImage).toBeVisible()
   })
 })
