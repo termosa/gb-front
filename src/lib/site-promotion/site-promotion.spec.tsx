f537f8712416bf9b156a9d068ea94c3a5bc66a66
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-09 12:01:20 +0300

    fix test for site promotion component

--- src/lib/site-promotion/site-promotion.spec.tsx
+++ src/lib/site-promotion/site-promotion.spec.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { render, waitFor } from '@testing-library/react'
 import SitePromotion from '.'
+import setCookie from '../set-cookie'
+import window from '../window'
+jest.mock('../window', () => ({}))
 
 describe('<SitePromotion />', () => {
+  beforeEach(() => {
+    Object.keys(window).forEach((key) => delete window[key])
+    Object.assign(window, { location: { hostname: 'localhost', search: '?promo=blackcat' }, document: { cookie: '' } })
+  })
+
   it('should render component', () => {
+    const expires = Date.now() + 3600 * 1000
+    setCookie('promo-expiration', expires, 1)
+    setCookie('c_promo', 'blackcat', 1)
+
     const { container } = render(<SitePromotion />)
-    waitFor(() => expect(container.querySelector('div').getAttribute('styled')).toBeTruthy())
+    waitFor(() => expect(container.querySelector('div')).toBeTruthy())
   })
 })
