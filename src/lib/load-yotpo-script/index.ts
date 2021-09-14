951d5b42a18a041afe5f8ee901444234ba7bad94
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-14 11:44:04 +0300

    refactor code, add function for scloll reviews

--- src/lib/load-yotpo-script/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './load-yotpo-script'
-export { loadYotpoScript as default } from './load-yotpo-script'
--- src/lib/load-yotpo-script/load-yotpo-script.ts
+++
@@ -1,9 +0,0 @@
-import loadScript from '../load-script'
-import useDefer, { Status } from 'use-defer'
-
-const YOTPO_SCRIPT_URL = '//staticw2.yotpo.com/LDoDRHPmIWai6MD5o41BGukBakwwgtNMncolHubV/widget.js'
-
-export function loadYotpoScript(): boolean {
-  const { status } = useDefer(() => loadScript(YOTPO_SCRIPT_URL), [], [])
-  return Status.SUCCESS === status
-}
