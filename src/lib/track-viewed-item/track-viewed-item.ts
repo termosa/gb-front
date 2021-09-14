288a6dceea775ff5a72f3cf8841a49ab6740c32d
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-09-14 17:43:11 +0300

    Double viewed item event

--- src/lib/track-viewed-item/track-viewed-item.ts
+++ src/lib/track-viewed-item/track-viewed-item.ts
@@ -1,6 +1,6 @@
 import klaviyo from '../klaviyo'
-import formatPrice from '../../modules/format-price'
 import { Product } from '../../modules/normalize-product'
+import formatPrice from '../../modules/format-price'
 
 export function trackViewedItem(product: Product): void {
   klaviyo('track', 'Viewed Product', {
@@ -8,5 +8,18 @@ export function trackViewedItem(product: Product): void {
     ProductID: product.product_id,
     ...(product.image && { ImageURL: product.image.src }),
     ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
+  })
+  klaviyo('trackViewedItem', {
+    Title: product.title,
+    ItemId: product.product_id,
+    ...(product.image && { ImageUrl: product.image.src }),
+    Url: `${window?.location.origin || ''}/products/${product.handle}`,
+    Metadata: {
+      ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
+      ...(product.variants[0]?.compare_at_price && {
+        CompareAtPrice: formatPrice(product.variants[0].compare_at_price),
+      }),
+      Brand: product.vendor,
+    },
   })
 }
