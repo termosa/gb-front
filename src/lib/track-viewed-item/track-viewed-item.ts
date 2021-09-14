75d9ecc3dc4b3ae03df8c367cc8a3b889b52e127
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-09-14 17:31:52 +0300

    Change viewed item Klaviyo event

--- src/lib/track-viewed-item/track-viewed-item.ts
+++ src/lib/track-viewed-item/track-viewed-item.ts
@@ -3,17 +3,10 @@ import formatPrice from '../../modules/format-price'
 import { Product } from '../../modules/normalize-product'
 
 export function trackViewedItem(product: Product): void {
-  klaviyo('trackViewedItem', {
-    Title: product.title,
-    ItemId: product.product_id,
-    ...(product.image && { ImageUrl: product.image.src }),
-    Url: `${window?.location.origin || ''}/products/${product.handle}`,
-    Metadata: {
-      ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
-      ...(product.variants[0]?.compare_at_price && {
-        CompareAtPrice: formatPrice(product.variants[0].compare_at_price),
-      }),
-      Brand: product.vendor,
-    },
+  klaviyo('track', 'Viewed Product', {
+    Name: product.title,
+    ProductID: product.product_id,
+    ...(product.image && { ImageURL: product.image.src }),
+    ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
   })
 }
