c5138a3c60534c5574483e00c7d24b950316c778
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-09-15 09:15:47 +0300

    Add rest of properties to first event

--- src/lib/track-viewed-item/track-viewed-item.ts
+++ src/lib/track-viewed-item/track-viewed-item.ts
@@ -3,24 +3,27 @@ import { Product } from '../../modules/normalize-product'
 import formatPrice from '../../modules/format-price'
 
 export function trackViewedItem(product: Product): void {
+  const variant = product.variants[0]
+
   klaviyo('track', 'Viewed Product', {
     Name: product.title,
     ProductID: product.product_id,
+    Brand: product.vendor,
+    URL: `${window?.location.origin || ''}/products/${product.handle}`,
     ...(product.image && { ImageURL: product.image.src }),
-    ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
-    ...(product.variants[0]?.compare_at_price && { CompareAtPrice: formatPrice(product.variants[0].compare_at_price) }),
+    ...(variant && { Price: formatPrice(variant.actual_price) }),
+    ...(variant?.compare_at_price && { CompareAtPrice: formatPrice(variant.compare_at_price) }),
   })
+
   klaviyo('trackViewedItem', {
     Title: product.title,
     ItemId: product.product_id,
-    ...(product.image && { ImageUrl: product.image.src }),
     Url: `${window?.location.origin || ''}/products/${product.handle}`,
     Metadata: {
-      ...(product.variants[0] && { Price: formatPrice(product.variants[0].actual_price) }),
-      ...(product.variants[0]?.compare_at_price && {
-        CompareAtPrice: formatPrice(product.variants[0].compare_at_price),
-      }),
       Brand: product.vendor,
+      ...(variant && { Price: formatPrice(variant.actual_price) }),
+      ...(variant?.compare_at_price && { CompareAtPrice: formatPrice(variant.compare_at_price) }),
     },
+    ...(product.image && { ImageUrl: product.image.src }),
   })
 }
