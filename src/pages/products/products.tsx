a9bc94d49c643765c2a43f006c49adcb7c897016
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-20 17:48:07 +0300

    fix load page collections, products

---
+++ src/pages/products/products.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import RemotePage from '../../lib/remote-page'
+
+export default function ProductsPage(): null | React.ReactElement {
+  return <RemotePage url="//www.fragrantjewels.com/products" />
+}
