5f37316ded677d5751954760f06c17b5eb11483b
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-20 15:54:30 +0300

    change interface collection

--- src/lib/collection-banner/collection-banner.tsx
+++ src/lib/collection-banner/collection-banner.tsx
@@ -3,10 +3,10 @@ import cn, { Argument as ClassName } from 'classnames'
 import styled, { css } from 'styled-components'
 import useScreenSize from '../use-screen-size'
 
-interface BannerCollection {
+interface CollectionBanner {
   handle: string
-  imageMobile: string
-  imageDesktop: string
+  compactBackgroundImage: string
+  backgroundImage: string
   title: string
   description: string
   styleTitleDesktop?: React.CSSProperties
@@ -22,14 +22,14 @@ type CollectionBannerProps = {
 
 export function CollectionBanner({ handle, className, style }: CollectionBannerProps): React.ReactElement | null {
   const useScreen = useScreenSize()
-  const bannerCollection = bannerCollections.find((item) => item.handle === handle)
+  const bannerCollection = collectionsBanner.find((item) => item.handle === handle)
   if (!bannerCollection) {
     return null
   }
   return useScreen.greaterThanMedium ? (
     bannerCollection.onlyDesktop ? (
       <>
-        <CollectionMobileImg desktop={bannerCollection.onlyDesktop} src={bannerCollection.imageDesktop} />
+        <CollectionMobileImg desktop={bannerCollection.onlyDesktop} src={bannerCollection.backgroundImage} />
         <CollectionMobileTitle style={bannerCollection.onlyDesktop ? bannerCollection.styleTitleDesktop : {}}>
           {bannerCollection.title}
         </CollectionMobileTitle>
@@ -38,7 +38,7 @@ export function CollectionBanner({ handle, className, style }: CollectionBannerP
         </CollectionMobileDescription>
       </>
     ) : (
-      <Wrapper className={cn(className)} style={style} backgroundImg={bannerCollection.imageDesktop}>
+      <Wrapper className={cn(className)} style={style} backgroundImg={bannerCollection.compactBackgroundImage}>
         <CollectionBannerWrapper>
           <CollectionTitle style={bannerCollection.styleTitleDesktop}>{bannerCollection.title}</CollectionTitle>
           <CollectionDescription style={bannerCollection.styleDescriptionDesktop}>
@@ -49,7 +49,7 @@ export function CollectionBanner({ handle, className, style }: CollectionBannerP
     )
   ) : (
     <Wrapper className={cn(className)} style={style}>
-      <CollectionMobileImg desktop={bannerCollection.onlyDesktop} src={bannerCollection.imageMobile} />
+      <CollectionMobileImg desktop={bannerCollection.onlyDesktop} src={bannerCollection.compactBackgroundImage} />
       <CollectionMobileTitle style={bannerCollection.onlyDesktop ? bannerCollection.styleTitleDesktop : {}}>
         {bannerCollection.title}
       </CollectionMobileTitle>
@@ -58,19 +58,19 @@ export function CollectionBanner({ handle, className, style }: CollectionBannerP
   )
 }
 
-const bannerCollections: BannerCollection[] = [
+const collectionsBanner: CollectionBanner[] = [
   {
     handle: 'bath-bombs',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bb-banner-2-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bb-banner-2-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bb-banner-2-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bb-banner-2-dt.jpg',
     title: 'Bath bombs',
     description: `Love your skin and it’ll love you back. Each bath bomb is packed with soothing shea butter and (surprise!) a stunning limited-edition ring.`,
     styleDescriptionDesktop: { color: '#000', fontWeight: 100 },
   },
   {
     handle: 'all-products',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/all-banner-2-1-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/all-banner-2-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/all-banner-2-1-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/all-banner-2-dt.jpg',
     title: 'Shop Everything',
     description: `Add a little calm to the chaos of life with our scented candles, bath bombs and sugar scrubs-discover limited-edition jewelry in each product!`,
     styleDescriptionDesktop: { color: '#fff', fontWeight: 100 },
@@ -78,8 +78,8 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'body-scrubs',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bs-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bs-banner-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bs-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bs-banner-dt.jpg',
     title: 'Body Scrubs',
     description: `Exfoliate your dry skin with our scented sugar scrubs. You'll step out of the shower with an added glow, soft skin and a stunning new ring.`,
     styleTitleDesktop: { color: '#000' },
@@ -87,8 +87,9 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'discover-your-birthstone',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/birthstone-banner-2-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/birthstone-banner-2-dt.jpg',
+    compactBackgroundImage:
+      'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/birthstone-banner-2-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/birthstone-banner-2-dt.jpg',
     title: 'Birthstone',
     description: `These candles and bath bombs make for the perfect gift — It’s two surprises in one!`,
     styleTitleDesktop: { color: '#000' },
@@ -96,8 +97,9 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'fantasy',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/fantasy-faves-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/fantasy-faves-banner-dt.jpg',
+    compactBackgroundImage:
+      'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/fantasy-faves-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/fantasy-faves-banner-dt.jpg',
     title: 'Fantasy Faves',
     description: `Escape to another world with our best selling fantasy collections.`,
     styleTitleDesktop: { color: '#fff' },
@@ -105,24 +107,25 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'gift-sets',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bundle-and-save-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bundle-and-save-banner-dt.jpg',
+    compactBackgroundImage:
+      'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bundle-and-save-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/bundle-and-save-banner-dt.jpg',
     title: 'Bundle & Save',
     description: `Get the full experience and best value when you shop bundles. You’re guaranteed to get a unique ring design inside each product!`,
     styleDescriptionDesktop: { color: '#000', fontWeight: 100 },
   },
   {
     handle: 'jewel-candles',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/candles-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/candles-banner-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/candles-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/candles-banner-dt.jpg',
     title: 'Jewel Candles',
     description: `Each scented candle burns clean and will transport you to a different world with rich aromas—strong enough to fill any room!`,
     styleDescriptionDesktop: { color: '#000', fontWeight: 100 },
   },
   {
     handle: 'satin',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners/collection-satin-2-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners/collection-satin-2-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners/collection-satin-2-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners/collection-satin-2-dt.jpg',
     title: 'New Birthstone Satin Collection',
     description: `Inside each candle, you'll discover 1 of 10 beautifully designed rings featuring a Swarovski®️ crystal birthstone. Collect all 10 for yourself or save them for the perfect decadent birthday gift.`,
     styleTitleDesktop: { color: '#ee67a0' },
@@ -131,24 +134,25 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'spooky',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/col-halloween-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/col-halloween-2-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/col-halloween-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/col-halloween-2-dt.jpg',
     title: '',
     description: `Feast your eyes on some of the most spook-tacular collections we've created. Stock up on these fall and wicked products before they disappear...`,
     styleDescriptionDesktop: { color: '#fff', fontWeight: 100 },
   },
   {
     handle: 'summer-collection',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/top-summer-picks-banner-2-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/top-summer-picks-banner-2-dt.jpg',
+    compactBackgroundImage:
+      'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/top-summer-picks-banner-2-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/top-summer-picks-banner-2-dt.jpg',
     title: 'Top Summer Picks',
     description: `Get your skin and home summertime ready with these refreshing summer scents, plus must-have ring styles.`,
     styleDescriptionDesktop: { color: '#000', fontWeight: 100 },
   },
   {
     handle: 'the-jewelry-store',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/jewelry-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/jewelry-banner-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/jewelry-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/jewelry-banner-dt.jpg',
     title: 'Jewelry',
     description: `Surprises are great, but sometimes you simply need that one jewel. Find that perfect design in our jewelry shop.`,
     styleTitleDesktop: { color: '#ffffff' },
@@ -156,8 +160,8 @@ const bannerCollections: BannerCollection[] = [
   },
   {
     handle: 'zodiac-collection-1',
-    imageMobile: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/zodiac-banner-mb.jpg',
-    imageDesktop: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/zodiac-banner-dt.jpg',
+    compactBackgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/zodiac-banner-mb.jpg',
+    backgroundImage: 'https://fragrantjewels-assets.s3.amazonaws.com/images/banners-2/zodiac-banner-dt.jpg',
     title: 'Zodiac',
     description: `These unique 925 sterling silver Zodiac sign rings are an astrology lovers dream. New releases every month.`,
     styleTitleDesktop: { color: '#ffffff' },
