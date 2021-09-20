424fe91753e86568f741d6b2ceffc5d354feaaac
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-20 18:29:28 +0300

    change fields module

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import RemotePage from '../../lib/remote-page'
+import RemotePage from '../lib/remote-page'
 
 export default function ProductsPage(): null | React.ReactElement {
   return <RemotePage url="//www.fragrantjewels.com/products/" />
