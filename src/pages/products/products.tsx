b34eabe83f6447e63331e452792f5efb459370c9
andrey.cherkashin <andrey.cherkashin@hygge.software>
2021-09-20 18:23:58 +0300

    change url for load page

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import RemotePage from '../../lib/remote-page'
 
 export default function ProductsPage(): null | React.ReactElement {
-  return <RemotePage url="//www.fragrantjewels.com/products" />
+  return <RemotePage url="//www.fragrantjewels.com/products/" />
 }
