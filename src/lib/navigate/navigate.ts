f450a548eb55d5c3dfff8a4492902853fa0ed7af
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-09-29 02:06:23 +0300

    Fix navigate function

--- src/lib/navigate/navigate.ts
+++ src/lib/navigate/navigate.ts
@@ -3,6 +3,6 @@ import window from '../window'
 
 const changeHref = (href: string) => window && (window.location.href = href)
 
-export function navigate(path: string): string | undefined {
-  return changeHref(createLink(path))
+export function navigate(url: string): string | undefined {
+  return changeHref(/^(?:https?:)?\/\//.test(url) ? url : createLink(url))
 }
